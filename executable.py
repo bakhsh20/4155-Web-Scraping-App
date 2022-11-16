c39023059f4430f57b9f6b64e4c389fd38711df0
wjarrel1 <79121373+wjarrel1@users.noreply.github.com>
2022-11-16 16:29:04 -0500

    Add files via upload
    Updated executable.py to work with beautifulsoup4 and rake

--- executable.py
+++ executable.py
@@ -1,5 +1,8 @@
 #import OS package
 import os
+import nltk
+
+
 
 #install rake_nltk onto system for keyword processor
 os.system('cmd /c "pip install rake_nltk"')
@@ -7,8 +10,17 @@ os.system('cmd /c "pip install rake_nltk"')
 #install scrapy for scrapers
 os.system('cmd /c "pip install scrapy"')
 
+#install beautifulsoup4
+os.system('cmd /c "pip install beautifulsoup4"')
+
+#install rake stopwords
+os.system('cmd /c "Python -m nltk.downloader stopwords"')
+
+#install punkt for rake
+os.system('cmd /c "Python -m nltk.downloader punkt"')
+
 #navigate to venv and activate it 
-os.chdir('./venv/Scripts')
+os.chdir('4155-Web-Scraping-App/venv/Scripts')
 os.system('cmd /c "activate"')
 
 #Step back to root, then navigate to spiders folder
@@ -17,10 +29,9 @@ os.chdir('..')
 os.chdir('./facultyscraper/facultyscraper/spiders')
 
 
-#run facultyspider, print to json
-os.system('cmd /c "python -m scrapy crawl facultyspider -O  faculty.json"')
+#run CCIDepartmentSpider, print to json
+os.system('cmd /c "python CCIDepartmentSpider.py"')
 
 #run descriptions-spider, print to json
-os.system('cmd /c "python -m scrapy crawl facultyDescriptionSpider -O descriptions.json"')
-
+#os.system('cmd /c "python -m scrapy crawl facultyDescriptionSpider -O descriptions.json"')
 
