83328a63e068aec8cfc10b51a0e48deb60825d07
bakhsh20 <dbakhsha@uncc.edu>
2022-11-17 18:11:59 -0500

    Fixed bugs inside the executable.py script

--- executable.py
+++ executable.py
@@ -2,8 +2,6 @@
 import os
 import nltk
 
-
-
 #install rake_nltk onto system for keyword processor
 os.system('cmd /c "pip install rake_nltk"')
 
@@ -19,18 +17,11 @@ os.system('cmd /c "Python -m nltk.downloader stopwords"')
 #install punkt for rake
 os.system('cmd /c "Python -m nltk.downloader punkt"')
 
-#navigate to venv and activate it 
-os.chdir('4155-Web-Scraping-App/venv/Scripts')
-os.system('cmd /c "activate"')
+os.chdir('facultyscraper/facultyscraper/spiders')
 
-#Step back to root, then navigate to spiders folder
-os.chdir('..')
-os.chdir('..')
-os.chdir('./facultyscraper/facultyscraper/spiders')
-
-
-#run CCIDepartmentSpider, print to json
+# run CCIDepartmentSpider, print to json
 os.system('cmd /c "python CCIDepartmentSpider.py"')
 
+os.system('cmd /c "python LiberalArtsDepartmentScraper.py"')
 
 
