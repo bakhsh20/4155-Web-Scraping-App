3396d1b416545884d4cac2399c039536c3a3aa1c
bakhsh20 <dbakhsha@uncc.edu>
2022-12-09 13:22:18 -0500

    Fixed bugs inside crawler

--- facultyscraper/facultyscraper/spiders/CHHSDepartmentScraper.py
+++ facultyscraper/facultyscraper/spiders/CHHSDepartmentScraper.py
@@ -13,28 +13,34 @@ rake_nltk_var = Rake(
 
 class PHSScraper(scrapy.Spider):
     # Name of spider
-    name = 'PHSSpider'
+    name = 'PHHSSpider'
 
     # Start URL of department faculty page.
-    start_urls = ['https://https://publichealth.charlotte.edu/research-service']
-
+    start_urls = ['https://publichealth.charlotte.edu/directory/faculty']
 
     # Get links for each of the page of the faculty member and scrapes data.
     def parse(self, response):
         # Loop through each link for each faculty member
         for link in response.css('a.button.button-green::attr(href)'):
             # Follow link. Callback function that scrapes faculty members page for information.
-            yield response.follow(finalLink.get(), callback=self.parseFacultyPage)
+            yield response.follow(link.get(), callback=self.followLink) 
+    
+    # Follow the secondary link on the page. This takes you to the faculty page that is on the department page. 
+    def followLink(self, response):
+        temp1 = response.css('div.field.field-name-field-directory-biography')
+        temp2 = temp1.css('div.field-items')
+        temp3 = temp2.css('div.field-item.even')
+        finalLink = temp3.css('a::attr(href)')
+        yield response.follow(finalLink.get(), callback=self.parseFacultyPage)
 
     # Function that scrapes faculty members page for information.
     def parseFacultyPage(self, response):
         # Create tempDescription to refine search for correct HTML
         tempDescription = response.css(
-            'div.field.field-name-field-directory-biography')
+            'div.details')
 
         # Clean the raw html to text
-        text = BeautifulSoup(tempDescription.css(
-            'div.field-item').get()).get_text()
+        text = BeautifulSoup(tempDescription.get()).get_text()
 
         # Extract keywords from the text content of each biography section
         rake_nltk_var.extract_keywords_from_text(text)
@@ -51,8 +57,8 @@ class PHSScraper(scrapy.Spider):
 
         # Creates object with properities for the name, title, descriptions(which is keyword list)...
         yield {
-            'department' : 'Public Health Services',
-            'name': response.css('h1.page-header::text').get(),
+            'department' : 'Public Health Sciences',
+            'name': response.css('div.name::text').get(),
             'title': "",
             'description': keywordList
         }
@@ -70,7 +76,7 @@ class APHCSScraper(scrapy.Spider):
         # Loop through each link for each faculty member
         for link in response.css('a.button.button-green::attr(href)'):
             # Follow link. Callback function that scrapes faculty members page for information.
-            yield response.follow(finalLink.get(), callback=self.parseFacultyPage)
+            yield response.follow(link.get(), callback=self.parseFacultyPage)
 
     # Function that scrapes faculty members page for information.
     def parseFacultyPage(self, response):
@@ -116,7 +122,7 @@ class SSWScraper(scrapy.Spider):
         # Loop through each link for each faculty member
         for link in response.css('a.button.button-green::attr(href)'):
             # Follow link. Callback function that scrapes faculty members page for information.
-            yield response.follow(finalLink.get(), callback=self.parseFacultyPage)
+            yield response.follow(link.get(), callback=self.parseFacultyPage)
 
     # Function that scrapes faculty members page for information.
     def parseFacultyPage(self, response):
@@ -154,7 +160,7 @@ class NursingScraper(scrapy.Spider):
     name = 'NursingSpider'
 
     # Start URL of department faculty page.
-    start_urls = ['https://nursing.charlotte.edu/directory/willie-m-abel-phd-rn-faha']
+    start_urls = ['https://nursing.charlotte.edu/about-us/faculty-and-staff']
 
 
     # Get links for each of the page of the faculty member and scrapes data.
@@ -162,7 +168,7 @@ class NursingScraper(scrapy.Spider):
         # Loop through each link for each faculty member
         for link in response.css('a.button.button-gray::attr(href)'):
             # Follow link. Callback function that scrapes faculty members page for information.
-            yield response.follow(finalLink.get(), callback=self.parseFacultyPage)
+            yield response.follow(link.get(), callback=self.parseFacultyPage)
 
     # Function that scrapes faculty members page for information.
     def parseFacultyPage(self, response):
@@ -204,8 +210,8 @@ process = CrawlerProcess(settings={
 })
 
 # Call scrapers and run
-process.crawl(PHHSSpider)
-process.crawl(APHCSSpider)
-process.crawl(SSWSpider)
-process.crawl(NursingSpider)
+process.crawl(PHSScraper)
+process.crawl(APHCSScraper)
+process.crawl(SSWScraper)
+process.crawl(NursingScraper)
 process.start()
